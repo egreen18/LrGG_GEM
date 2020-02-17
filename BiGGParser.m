5214781c55a2104faa849e1abcdce7856c84e25a
Ethan Green <egreen18@rams.colostate.edu>
2020-02-17 15:43:06 -0700

    Used BiGGParser to apply provided information to updated the model consistency.

--- BiGGParser.m
+++ BiGGParser.m
@@ -8,9 +8,12 @@
 load MetParsed.mat 
 load BiGGmetData_01_04_2017.mat
 load LrGG_Model.mat
+modelorigin = model;
 
 %% Pullling data and exporting to LrGG Model
 [yn, id] = ismember(mets, BiGGmetData(:, 2));
 model.metCharges(yn, 1) = str2double(BiGGmetData(id(yn), 7));
 [yn, id] = ismember(mets, BiGGmetData(:, 2));
 model.metFormulas(yn, 1) = BiGGmetData(id(yn), 6);
+[yn, id] = ismember(mets, BiGGmetData(:, 2));
+model.mets(yn, 1) = BiGGmetData(id(yn), 1);
